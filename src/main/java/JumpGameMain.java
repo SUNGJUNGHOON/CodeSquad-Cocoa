278e7350d7a63492e86927504319854aefc96f69
SUNGJUNGHOON <corqkrtk1@gmail.com>
2021-11-02 17:13:07 +0900

    포문을 이용한 주사위게임 구현

---
+++ src/main/java/JumpGameMain.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class JumpGameMain {
+    private int rollTheDice;
+    private int[] jumpingDistance;
+
+    public JumpGameMain(int name, int rollTheDice) {
+        this.rollTheDice = rollTheDice;
+        jumpingDistance = new int[name];
+    }
+
+    public void person() {
+        for (int i = 0; i < jumpingDistance.length; i++) {
+            dropDice();
+        }
+    }
+
+    private void dropDice() {
+        for (int j = 0; j < rollTheDice; j++) {
+            randomDice();
+    }
+        System.out.println();
+}
+
+    private void randomDice() {
+        Random random = new Random();
+        if (random.nextInt(6) >= 4) {
+            System.out.print("ㅡ");
+        }
+    }
+}
+
